2226b6c546ad0c1a93f0432e50e5782a17d43008
miwanoff <marina.iva.noffa@gmail.com>
2025-05-25 14:55:52 +0300

    Add src and dist

--- js/goods.js
+++
@@ -1,7 +0,0 @@
-$("button").click(function () {
-  $.getJSON("books.json", function (result) {
-    $.each(result, function (i, field) {
-      $("#books").append(i + " " + field.title + "<br>");
-    });
-  });
-});
