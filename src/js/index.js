f9af739569a358948087060b3553785a0195bfd3
miwanoff <marina.iva.noffa@gmail.com>
2025-05-25 15:24:43 +0300

    Add css

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import basket from "./modules/basket";
 import goods from "./modules/goods";
+import "../css/style.css";
+import "../css/basket.css";
+import "../css/goods.css";
 window.addEventListener("DOMContentLoaded", function () {
   basket();
   goods();
